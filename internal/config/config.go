5440decee4465812f1068133ea38bc50550d109b
AlexW <warenik.work@gmail.com>
2026-03-01 15:22:54 +0300

    increment 4 fix bug

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,7 @@
 package config
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
-	"os"
 )
 
 type Config struct {
@@ -21,35 +18,16 @@ func LoadConfig() *Config {
 		PollInterval:   2,
 	}
 
-	//получаем конфиг из файла json
-	err := loadFromFile(cfg, "config.json")
-	if err != nil {
-		//TODO: прикрутить логировние ошибки тут
-		fmt.Println("Ошибка парсинга конфига:", err)
-	}
-
 	//получаем конфиг из командной строки
 	loadFromCLI(cfg)
 
 	return cfg
 }
 
-func loadFromFile(cfg *Config, path string) error {
-	if file, err := os.Open(path); err == nil {
-		defer file.Close()
-		if err = json.NewDecoder(file).Decode(cfg); err != nil {
-			return err
-		}
-		return nil
-	} else {
-		return err
-	}
-}
-
 func loadFromCLI(cfg *Config) {
 	flag.StringVar(&cfg.ServerAddr, "a", cfg.ServerAddr, "HTTP address")
-	flag.IntVar(&cfg.ReportInterval, "ri", cfg.ReportInterval, "Reporting interval in seconds")
-	flag.IntVar(&cfg.PollInterval, "pi", cfg.PollInterval, "Polling interval in seconds")
+	flag.IntVar(&cfg.ReportInterval, "r", cfg.ReportInterval, "Reporting interval in seconds")
+	flag.IntVar(&cfg.PollInterval, "p", cfg.PollInterval, "Polling interval in seconds")
 
 	flag.Parse()
 }
